c8a5a4da7859ffa9057d57673838401fac7a8190
Alex Burton <alex.burton@LC-LTM026.local>
2016-09-29 10:58:37 +0100

    quick seed updates for user

--- server/seed/data/users.js
+++ server/seed/data/users.js
@@ -9,9 +9,13 @@ var users = [
     password: 'Admintest1!',
     store_id: '101',
     achievements : [
-      { "earned" : true, "_id" : "762fa052-19f6-48a7-9704-701362885ef5" }, 
-      { "earned" : false, "_id" : "33634f75-3f47-4679-a064-2affe1620513" }
+      { earned: true, _id: '762fa052-19f6-48a7-9704-701362885ef5' }, 
+      { earned: false, _id: '33634f75-3f47-4679-a064-2affe1620513' }
     ],
+    personalDetails: {
+        firstName: "test",
+        lastName: "tester"
+    },
     role: 'admin'
   },
   {
@@ -24,8 +28,13 @@ var users = [
     password: 'Usertest1!',
     store_id: '102',
     achievements : [
-      { "earned" : false, "_id" : "b391f053-c4d3-4a71-b2e7-39116e662d68" }
-    ]
+      { earned: false, _id: 'b391f053-c4d3-4a71-b2e7-39116e662d68' }
+    ],
+    personalDetails: {
+        firstName: "test",
+        lastName: "tester"
+    },
+    role: 'user'
   }
 ];
 
